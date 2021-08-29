cf2802d9b2897a124f55900a5a03080cea8c10c4
neilrush <neileorushio@gmail.com>
2021-08-29 08:06:33 -0500

    update color scheme and hide testing by default

--- Responsive Web Design Projects/4. Technical Documentation/Technical Documentation.js
+++ Responsive Web Design Projects/4. Technical Documentation/Technical Documentation.js
@@ -1,24 +1,45 @@
-const btn = document.querySelector("#dark-mode-button");
+const btn = document.getElementById("dark-mode-button");
 const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+var colorScheme = window.matchMedia("(prefers-color-scheme: dark)") ? "dark" : "light";
 
 const currentTheme = localStorage.getItem("theme");
 if (currentTheme == "dark") {
+  document.documentElement.style.setProperty("color-scheme", "dark");
   document.body.classList.toggle("dark-theme");
+  colorScheme = "dark";
 } else if (currentTheme == "light") {
+  document.documentElement.style.setProperty("color-scheme", "light");
   document.body.classList.toggle("light-theme");
+  colorScheme = "light";
 }
 
-btn.addEventListener("click", function () {
+btn.addEventListener("click", function() {
+  var theme = "";
   if (prefersDarkScheme.matches) {
     document.body.classList.toggle("light-theme");
-    var theme = document.body.classList.contains("light-theme")
+    theme = document.styleSheets.classList.contains("light-theme")
       ? "light"
       : "dark";
+      toggleColorScheme();
   } else {
     document.body.classList.toggle("dark-theme");
-    var theme = document.body.classList.contains("dark-theme")
+    theme = document.body.classList.contains("dark-theme")
       ? "dark"
       : "light";
+    toggleColorScheme();
   }
   localStorage.setItem("theme", theme);
 });
+
+function toggleColorScheme() {
+  if(colorScheme == "dark") {
+    colorScheme = "light";
+    document.documentElement.style.setProperty("color-scheme", "light");
+  }
+  else {
+    colorScheme = "dark"
+    document.documentElement.style.setProperty("color-scheme", "dark");
+  }
+}
+
+/* From https://css-tricks.com/a-complete-guide-to-dark-mode-on-the-web/ */
